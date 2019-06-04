4ad04d9db4c3d9b22e5a6f71b4637c37f2f8c91e
Khgds1 <khgds1@hotmail.com>
2019-06-04 16:31:30 -0300

    atualizando home

---
+++ src/app/profile-artist/profile-artist.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-artist',
+  templateUrl: './profile-artist.page.html',
+  styleUrls: ['./profile-artist.page.scss'],
+})
+export class ProfileArtistPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
